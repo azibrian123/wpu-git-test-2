c81f86e7d539fec3e6ec3145556da7ee726c9be4
azibrian123 <azibrian123@gmail.com>
2024-01-02 17:38:43 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam dolorum voluptatem odio, ipsa, dolorem est incidunt fugit, laborum voluptate neque hic cupiditate! Voluptates sunt consequuntur repudiandae laudantium nesciunt sint ab.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 
 </html>
